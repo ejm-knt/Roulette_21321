d209740984ac543c946c54223808604b66ede5c5
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-11-29 14:49:02 +0900

    Test.csの追加

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,12 +7,16 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
