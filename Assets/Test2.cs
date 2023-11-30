4b29f6e0be7652017b52f178d5aacb9f5aa1a8cc
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-11-30 10:54:11 +0900

    Test.cs コメント追加 ・各定義で何が行われいるかのコメントを追加

--- Assets/Test2.cs
+++ Assets/Test2.cs
@@ -7,12 +7,16 @@ public class Test2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vecter2 startPos = new Vecter2(2.0f, 1.0f);
-        Vecter2 endPos = new Vecter2(8.0f, 5.0f);
-        Vecter2 dir = endPos - startPos;
-        Debug.Log(dir);
+        Vector2 startPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos - startPos; //! x(8-2) , y(5-1) -> dir(x:6.0f, y:4.0f) ということが起きてる
+        Debug.Log(dir); //! dir(x:6.00, y:4.00)
 
-        float len = dir.magnitude;
+        //! 三平方の定理を使って斜辺の長さ(ベクトル)を求める -> magnitude
+        //! (6^2 + 4^2) = len(z^2) -> z^2 is ikutsu?
+        //! 36 + 16 -> 52 -> 7.211... -> start-endのベクトル
+        float len = dir.magnitude; 
+        
         Debug.Log(len);
     }
 
