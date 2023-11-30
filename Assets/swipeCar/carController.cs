1f2dec517e9478e8532287a5e427b48f60c9b81d
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-11-30 13:22:08 +0900

    Assets\swipeCar\carController.cs　新規作成
    ・下記処理をスクリプトで追加
    　・左クリックで加速
    　・左クリックを離したら減速

--- Assets/swipeCar/carController.cs
+++ Assets/swipeCar/carController.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class carController : MonoBehaviour
 {
     // Start is called before the first frame update
+    float speed = 0;
+
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))        //* 左クリック
+        {
+            this.speed = 0.2f;                  //* 初速度
+        }
+
+        transform.Translate(this.speed, 0, 0);  //* 移動
+        this.speed *= 0.98f;                    //* 減速
     }
 }
