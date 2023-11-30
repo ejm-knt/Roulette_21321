0dde77000949bb0ab79c5ff14c2e85962d9bd142
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-11-30 14:37:53 +0900

    Assets\swipeCar\carController.cs　機能追加 ・ボタンクリックでの移動からマウススワイプした距離を元に移動に変更

--- Assets/swipeCar/carController.cs
+++ Assets/swipeCar/carController.cs
@@ -1,26 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class carController : MonoBehaviour
+public class CarController : MonoBehaviour
 {
     // Start is called before the first frame update
     float speed = 0;
+    Vector2 startPos;
 
     void Start()
     {
         Application.targetFrameRate = 60;
+            // 初期位置の取得（のつもり）
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))        //* 左クリック
+        if(Input.GetMouseButtonDown(0))
         {
-            this.speed = 0.2f;                  //* 初速度
+            this.startPos = Input.mousePosition;
         }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            this.speed = swipeLength / 500.0f;
+        }
+
+        // if (Input.GetMouseButton(2))        //* 左クリック
+        // {
+        //     this.speed = 0.2f;                  //* 初速度
+        // }
 
         transform.Translate(this.speed, 0, 0);  //* 移動
         this.speed *= 0.98f;                    //* 減速
     }
+
 }
