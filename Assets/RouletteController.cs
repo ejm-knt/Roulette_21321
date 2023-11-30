5daadea4c729444c6b8e72486df4af6a518a19d3
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-11-30 11:25:24 +0900

    RouletteController.cs 追記 ・Roulette止める用のお試しelseとそのコメントを追記

--- Assets/RouletteController.cs
+++ Assets/RouletteController.cs
@@ -17,10 +17,18 @@ public class RouletteController : MonoBehaviour
     void Update()
     {
         //! マウスが押されたら回転速度を設定
-        if(Input.GetMouseButtonDown(0))
+        //? Input.@@@ で 〇〇を入力すると となる。今回はMouseBottonDown(マウスを押した瞬間)
+        //? ちなみに、MBotton(マウスを押してる間)、MBDown(マウスを押した瞬間)、MBUp(マウスを離した瞬間)
+        //? ちなみのちなみに、MBD(0) の 0 は左クリックのナンバリングを指す。
+        if(Input.GetMouseButton(0))
         {
             this.rotSpeed = 10;
         }
+        //! 遊びで追加。GMBでかつif-else書くと、押してる時に回って、離すと止まるようになる。
+        // else
+        // {
+        //     this.rotSpeed = 0;
+        // }
 
         //! 回転速度分、ルーレットを回転させる
         transform.Rotate(0, 0, this.rotSpeed);
