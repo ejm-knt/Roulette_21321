6b4e63f5e25bdcfc9bde32ac8559566d16cfe657
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-12-04 13:22:44 +0900

    猫JumpAnim追加 ・JumpAnim新規作成 ・アニメーション編集 ・PControllerでJump制御追加

--- Assets/ClimbCloud/PlayerControllerCC.cs
+++ Assets/ClimbCloud/PlayerControllerCC.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class PlayerControllerCC : MonoBehaviour
@@ -24,8 +25,9 @@ public class PlayerControllerCC : MonoBehaviour
     void Update()
     {
         // ジャンプ
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
         {
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
         // 左右移動
@@ -48,11 +50,25 @@ public class PlayerControllerCC : MonoBehaviour
         }
 
         //プレイヤーの速度に応じてアニメーション速度を変える
-        this.animator.speed = speedx / 2.0f;
+        if (this.rigid2D.velocity.y == 0)
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+        }
+
+        // 画面外に出た場合は最初から
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("ゴール");
+        SceneManager.LoadScene("ClearScene");
     }
 }
