e5836b9856d12199e17c627626ef175cb187d27d
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-12-04 09:39:35 +0900

    climbCloud 機能追加 ・猫が移動できるようになった ・猫が移動時に絵が反転するようになった

--- Assets/ClimbCloud/PlayerControllerCC.cs
+++ Assets/ClimbCloud/PlayerControllerCC.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerControllerCC : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody2D rigid2D;
     float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
 
     void Start()
     {
@@ -17,8 +21,28 @@ public class PlayerControllerCC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
+        // ジャンプ
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+        // 左右移動
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        // プレイヤー速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        // スピード制限
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
         }
     }
 }
